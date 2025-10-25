8ff768a0232ece3175fa718dac83383a787fe368
Jerome Lachaud <jerome.lachaud@gmail.com>
2025-10-25 12:47:01 +0200

    Refactor ESLint configuration and update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,25 @@
 const promise = require('eslint-plugin-promise')
+const js = require('@eslint/js')
+const globals = require('globals')
 
-module.exports = {
-  languageOptions: {
-    globals: {
-      es6: true,
-      node: true,
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2017,
+      sourceType: 'commonjs',
+      globals: Object.assign({}, globals.es2015, globals.node),
+    },
+    plugins: {
+      promise,
+    },
+    rules: {
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      'no-console': 'off',
+      'promise/catch-or-return': 'error',
     },
   },
-  plugins: {
-    promise,
-  },
-  extends: 'eslint:recommended',
-  parserOptions: {
-    ecmaVersion: 2017,
-  },
-  rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-    'no-console': 'off',
-    'promise/catch-or-return': 'error',
-  },
-}
+]
