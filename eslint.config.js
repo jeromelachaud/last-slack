f5a6324d15436150abfd9bd760136128fe8213a2
Jerome Lachaud <jerome.lachaud@gmail.com>
2025-10-25 12:40:30 +0200

    Migrate ESLint configuration from .eslintrc.json to eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  plugins: ['promise'],
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'no-console': 'off',
+    'promise/catch-or-return': 'error',
+  },
+}
